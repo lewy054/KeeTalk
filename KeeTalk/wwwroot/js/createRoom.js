a71751b9bdcb543c942b207f260395f3fa03f4e4
Norbert Lewicz <lewy054@gmail.com>
2021-05-30 04:36:11 +0200

    Add card preview Card preview when creating a new room

---
+++ KeeTalk/wwwroot/js/createRoom.js
@@ -0,0 +1,27 @@
+﻿window.addEventListener("load", startup, false);
+
+function startup() {
+    var inputColor = document.querySelector("#inputColor");
+    var defaultColor = "#a2ff7a";
+    inputColor.value = defaultColor;
+    inputColor.addEventListener("input", changeColor, false);
+    inputColor.select();
+}
+function changeColor(event) {
+    console.log('test')
+    document.querySelectorAll("#checkColor").forEach(function (p) {
+        console.log('test')
+        console.log(event.target.value)
+        checkColor.style.backgroundColor = event.target.value;
+    });
+}
+function changeDescription(input) {
+    console.log("change changeDescription")
+    checkDescription = document.getElementById("checkDescription");
+    checkDescription.innerHTML = input.value;
+}
+function changeName(input) {
+    console.log("change changeName")
+    checkName = document.getElementById("checkName");
+    checkName.innerHTML = input.value;
+}
